331a606bebeab437523dd6b658875e6d9892a94c
coleh <cole.hansen.4856@gmail.com>
2021-08-26 11:10:47 -0500

    heroku adjustments

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,12 +1,21 @@
 <?php
 
+$host = $username = $password = $dbname = '';
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+if (isset($url["host"]) && isset($url["user"]) && isset($url["pass"]) && isset($url["path"])) {
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+}
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=' . $host .';dbname=' . $dbname,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
         ],
         'mailer' => [
