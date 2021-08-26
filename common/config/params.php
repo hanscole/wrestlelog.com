5011c20d1be080e96bcec4c504303259d00f4c74
coleh <cole.hansen.4856@gmail.com>
2021-08-26 14:38:46 -0500

    change sender email address

--- common/config/params.php
+++ common/config/params.php
@@ -2,8 +2,8 @@
 return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'senderEmail' => 'noreply@wrestlelog.com',
+    'senderName' => 'Wrestle Log',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
 ];
